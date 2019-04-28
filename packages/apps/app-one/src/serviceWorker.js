5e755e867c0ec1b781b7f3a0e29cf5de91eef0db
F1LT3R <al@pwn.io>
2019-04-28 02:12:54 -0400

    Upgrade CRA/react-scripts to 3. Fix tests.

--- packages/apps/app-one/src/serviceWorker.js
+++ packages/apps/app-one/src/serviceWorker.js
@@ -60,9 +60,10 @@ function registerValidSW(swUrl, config) {
 		.then(registration => {
 			registration.onupdatefound = () => {
 				const installingWorker = registration.installing;
-				if (installingWorker == null) {
+				if (installingWorker === null) {
 					return;
 				}
+
 				installingWorker.onstatechange = () => {
 					if (installingWorker.state === 'installed') {
 						if (navigator.serviceWorker.controller) {
@@ -106,7 +107,7 @@ function checkValidServiceWorker(swUrl, config) {
 			const contentType = response.headers.get('content-type');
 			if (
 				response.status === 404 ||
-				(contentType != null &&
+				(contentType !== null &&
 					contentType.indexOf('javascript') === -1)
 			) {
 				// No service worker found. Probably a different app. Reload the page.
