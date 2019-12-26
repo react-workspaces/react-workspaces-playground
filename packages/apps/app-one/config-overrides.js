0f38801cfc8535e16d3421a70bf3c8c898613126
F1LT3R <al@pwn.io>
2019-12-25 21:10:12 -0500

    Ant Design working with customize-cra and react-app-rewired.

--- packages/apps/app-one/config-overrides.js
+++ packages/apps/app-one/config-overrides.js
@@ -1,5 +1,9 @@
-module.exports = function override(config, env) {
-	//do stuff with the webpack config...
-	// process.exit();
-	return config;
-};
+const {override, fixBabelImports} = require('customize-cra');
+
+module.exports = override(
+	fixBabelImports('import', {
+		libraryName: 'antd',
+		libraryDirectory: 'es',
+		style: 'css'
+	})
+);
