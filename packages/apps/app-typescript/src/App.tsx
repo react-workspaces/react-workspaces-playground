6dc1bc57c6f23965d34521c17d9fdf261acf56c3
F1LT3R <al@pwn.io>
2019-12-26 20:40:41 -0500

    Fix app-typescript test. Fix .editorconfig.

--- packages/apps/app-typescript/src/App.tsx
+++ packages/apps/app-typescript/src/App.tsx
@@ -25,12 +25,12 @@ const App: React.FC = () => {
         </h1>
         <h2>Hot Reload Your React TypeScript Workspaces</h2>
         <p className="body">
-          <code className="file">packages/apps/app-one/src/App.js</code>
+          <code className="file">packages/apps/app-one/src/App.tsx</code>
           <code className="file">
-            packages/components/src/CompOne/CompOne.js
+            packages/components/src/CompOne/CompOne.tsx
           </code>
           <code className="file">
-            packages/components/src/CompTwo/CompTwo.js
+            packages/components/src/CompTwo/CompTwo.tsx
           </code>
         </p>
         <div className="components">
