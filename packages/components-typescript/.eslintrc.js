425b518953f4486516480fb30b7ea219a5156d0b
Jørn André Myrland <j@myrland.nu>
2020-01-16 10:12:53 +0100

    Fix related to issue #29
    + Adds .env file to extend eslint
    + Adjusted eslintrc.js to be based on CRA by default

--- packages/components-typescript/.eslintrc.js
+++ packages/components-typescript/.eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    // Extend on the config used in `react-scripts`
+    require.resolve('eslint-config-react-app'),
+    // If you want extra rules/extensions, it can be added here:
+    // 'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    // 'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
