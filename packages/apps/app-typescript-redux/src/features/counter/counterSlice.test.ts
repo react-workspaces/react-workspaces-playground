325d846c8a1ef8932d5d0b9db8acdac650f6390b
Chris Canal <chris@stormideas.com>
2020-04-25 16:08:28 +0530

    redux-app: Correctly adds initial app

---
+++ packages/apps/app-typescript-redux/src/features/counter/counterSlice.test.ts
@@ -0,0 +1,17 @@
+import reducer, { increment, decrement, incrementByAmount, incrementAsync } from './counterSlice';
+
+test('initialState', () => {
+  expect(reducer(undefined, {})).toEqual({ value: 0 });
+});
+
+test('increment', () => {
+  expect(reducer(undefined, increment)).toEqual({ value: 1 });
+});
+
+test('decrement', () => {
+  expect(reducer(undefined, decrement)).toEqual({ value: -1 });
+});
+
+test('incrementByAmount', () => {
+  expect(reducer(undefined, incrementByAmount(100))).toEqual({ value: 100 });
+});
